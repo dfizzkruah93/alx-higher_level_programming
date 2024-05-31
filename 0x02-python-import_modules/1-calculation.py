01621e157ee5748b1d99cc005d4e60e157ea0e14
dfizzkruah93 <dfizzkruah@gmail.com>
2024-05-31 02:09:36 -0700

    create file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+def main():
+    a = 10
+    b = 5
+    print("a + b = {}".format(add(a, b)))
+    print("a - b = {}".format(sub(a, b)))
+    print("a * b = {}".format(mul(a, b)))
+    print("a / b = {}".format(div(a, b)))
+
+if __name__ == "__main__":
+    main()
